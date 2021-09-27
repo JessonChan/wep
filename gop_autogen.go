8f8725a875869cabcd0c35237fde512fd22d4933
JessonChan <chinachendongpo@gmail.com>
2021-09-27 17:09:02 +0800

    add gop Join func

--- gop_autogen.go
+++ gop_autogen.go
@@ -1,10 +1,35 @@
 package wep
 
-import fmt "fmt"
+import (
+	fmt "fmt"
+	reflect "reflect"
+)
 
-func Run(args ...string) {
-//line /tmp/wep/wep.gop:4
-	addr := append(args, ":8080")[0]
-//line /tmp/wep/wep.gop:5
-	fmt.Println("go" + addr)
+func Run() {
+}
+func Join(slices interface {
+}, sep string) string {
+//line /tmp/wep/wep.gop:9
+	full := ""
+//line /tmp/wep/wep.gop:10
+	if reflect.TypeOf(slices).Kind() == reflect.Slice {
+//line /tmp/wep/wep.gop:11
+		s := reflect.ValueOf(slices)
+//line /tmp/wep/wep.gop:12
+		for
+//line /tmp/wep/wep.gop:12
+		i := 0; i < s.Len();
+//line /tmp/wep/wep.gop:12
+		i++ {
+//line /tmp/wep/wep.gop:13
+			full += fmt.Sprint(s.Index(i))
+//line /tmp/wep/wep.gop:14
+			if i < s.Len()-1 {
+//line /tmp/wep/wep.gop:15
+				full += sep
+			}
+		}
+	}
+//line /tmp/wep/wep.gop:19
+	return full
 }
