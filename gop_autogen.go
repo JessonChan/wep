799ba8b2fbc5bbd6e1ae08630ea79272aa2f443e
JessonChan <chinachendongpo@gmail.com>
2021-10-20 10:20:50 +0800

    add each func

--- gop_autogen.go
+++ gop_autogen.go
@@ -5,31 +5,51 @@ import (
 	reflect "reflect"
 )
 
-func Run() {
-}
 func Join(slices interface {
-}, sep string) string {
-//line /tmp/wep/wep.gop:9
-	full := ""
-//line /tmp/wep/wep.gop:10
+}, sep string) (full string) {
+//line /Users/jessonchan/code/golang/wep/wep.gop:6
 	if reflect.TypeOf(slices).Kind() == reflect.Slice {
-//line /tmp/wep/wep.gop:11
+//line /Users/jessonchan/code/golang/wep/wep.gop:7
 		s := reflect.ValueOf(slices)
-//line /tmp/wep/wep.gop:12
+//line /Users/jessonchan/code/golang/wep/wep.gop:8
 		for
-//line /tmp/wep/wep.gop:12
+//line /Users/jessonchan/code/golang/wep/wep.gop:8
 		i := 0; i < s.Len();
-//line /tmp/wep/wep.gop:12
+//line /Users/jessonchan/code/golang/wep/wep.gop:8
 		i++ {
-//line /tmp/wep/wep.gop:13
+//line /Users/jessonchan/code/golang/wep/wep.gop:9
 			full += fmt.Sprint(s.Index(i))
-//line /tmp/wep/wep.gop:14
+//line /Users/jessonchan/code/golang/wep/wep.gop:10
 			if i < s.Len()-1 {
-//line /tmp/wep/wep.gop:15
+//line /Users/jessonchan/code/golang/wep/wep.gop:11
 				full += sep
 			}
 		}
 	}
-//line /tmp/wep/wep.gop:19
-	return full
+//line /Users/jessonchan/code/golang/wep/wep.gop:15
+	return
+}
+func Each(slices interface {
+}, sep string) (full string) {
+//line /Users/jessonchan/code/golang/wep/wep.gop:18
+	if reflect.TypeOf(slices).Kind() == reflect.Slice {
+//line /Users/jessonchan/code/golang/wep/wep.gop:19
+		s := reflect.ValueOf(slices)
+//line /Users/jessonchan/code/golang/wep/wep.gop:20
+		for
+//line /Users/jessonchan/code/golang/wep/wep.gop:20
+		i := 0; i < s.Len();
+//line /Users/jessonchan/code/golang/wep/wep.gop:20
+		i++ {
+//line /Users/jessonchan/code/golang/wep/wep.gop:21
+			full += fmt.Sprint(s.Index(i))
+//line /Users/jessonchan/code/golang/wep/wep.gop:22
+			if i < s.Len()-1 {
+//line /Users/jessonchan/code/golang/wep/wep.gop:23
+				full += sep
+			}
+		}
+	}
+//line /Users/jessonchan/code/golang/wep/wep.gop:27
+	return
 }
